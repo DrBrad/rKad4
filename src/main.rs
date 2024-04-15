b9ad90b22d4a88c55db879ee257a7cbc93dfdf3c
DrBrad <brad@bradeagle.com>
2024-04-15 15:22:27 -0600

    Send data

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,9 @@ use std::net::{SocketAddr, UdpSocket};
 
 fn main() -> std::io::Result<()> {
     {
-        let addr = SocketAddr::from(([127, 0, 0, 1], 0));
-
         let send = SocketAddr::from(([127, 0, 0, 1], 1080));
 
-        let socket = UdpSocket::bind(addr)?;//"127.0.0.1:0")?;
+        let socket = UdpSocket::bind(SocketAddr::from(([127, 0, 0, 1], 0)))?;//"127.0.0.1:0")?;
         let buf = "asdasdasdasd".as_bytes();;
         socket.send_to(&buf, &send)?;
 
