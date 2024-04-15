6a35ac94ce12f8958ada1f84ff5a7fd629aa42c4
DrBrad <brad@bradeagle.com>
2024-04-15 15:08:20 -0600

    Send data

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,20 @@
-use std::net::UdpSocket;
+use std::net::{SocketAddr, UdpSocket};
 
 fn main() -> std::io::Result<()> {
     {
-        let socket = UdpSocket::bind("127.0.0.1:34254")?;
+        let addr = SocketAddr::from(([127, 0, 0, 1], 0));
 
-        // Receives a single datagram message on the socket. If `buf` is too small to hold
-        // the message, it will be cut off.
+        let send = SocketAddr::from(([127, 0, 0, 1], 1080));
+
+        let socket = UdpSocket::bind(addr)?;//"127.0.0.1:0")?;
+        let buf = "asdasdasdasd".as_bytes();;
+        socket.send_to(&buf, &send)?;
+
+        /*
         let mut buf = [0; 65535];
         let (amt, src) = socket.recv_from(&mut buf)?;
-
-        // Redeclare `buf` as slice of the received data and send reverse data back to origin.
-        let buf = &mut buf[..amt];
-        buf.reverse();
-        socket.send_to(buf, &src)?;
-    } // the socket is closed here
+        */
+    }
     Ok(())
 }
 
