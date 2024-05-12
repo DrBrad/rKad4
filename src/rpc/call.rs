0b9e1d17854afd579a6c02b3a41e185f3c072795
DrBrad <brad@bradeagle.com>
2024-05-12 12:57:34 -0600

    Call

--- src/rpc/call.rs
+++ src/rpc/call.rs
@@ -1,8 +1,20 @@
+use crate::messages::inter::message_base::MessageBase;
+use crate::utils::node::Node;
 
-pub struct Call {
-
+pub struct Call<'a> {
+    message: &'a dyn MessageBase,
+    node: Option<Node>,
+    //callback: callback
+    sent_time: u64
 }
 
-impl Call {
+impl<'a> Call<'a> {
 
+    pub fn new(message: &'a dyn MessageBase) -> Self {
+        Self {
+            message,
+            node: None,
+            sent_time: 0
+        }
+    }
 }
