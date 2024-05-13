fc806de5658486bebbc47ca45dec2126843eb419
DrBrad <brad@bradeagle.com>
2024-05-13 09:02:16 -0600

    Call

--- src/rpc/call.rs
+++ src/rpc/call.rs
@@ -17,4 +17,35 @@ impl<'a> Call<'a> {
             sent_time: 0
         }
     }
+
+    pub fn get_message(&self) -> &dyn MessageBase {
+        self.message
+    }
+
+    pub fn has_node(&self) -> bool {
+        self.node.is_some()
+    }
+
+    pub fn set_node(&mut self, node: Node) {
+        self.node = Some(node);
+    }
+
+    pub fn get_node(&self) -> Node {
+        self.node.unwrap()
+    }
+
+    //response callback - has, set, get
+
+    pub fn set_sent_time(&mut self, sent_time: u64) {
+        self.sent_time = self.sent_time;
+    }
+
+    pub fn get_sent_time(&self) -> u64 {
+        self.sent_time
+    }
+
+    pub fn is_stalled(&self) -> bool {
+        false
+        //return (now-sentTime > STALLED_TIME);
+    }
 }
